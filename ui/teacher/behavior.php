1164bd49743367c8539c6c91699b23a259f37fbe
kansiri <42263656+kansiri@users.noreply.github.com>
2019-03-25 23:29:12 +0700

    karnsiri upload

---
+++ ui/teacher/behavior.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>CAAS TEACHER</title>
+</head>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Marvel');
+body {
+    font-family: 'Marvel', sans-serif;
+}
+</style>
+<body style="background-color:#A6A6A6;">
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color:#282D3D; color:#FFFFFF;">
+            <ul class="nav justify-content-center">
+            <a class="navbar-brand" href="teacherhome.php" style="color:#FFFFFF;">CAAS</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="color:#FFFFFF;">Student Management</a>
+                    <div class="dropdown-menu" style="color:#FFFFFF;">
+                        <a class="dropdown-item" href="behavior.php">Behavior Management</a>
+                        <a class="dropdown-item" href="score.php">Score Management</a>
+                        <a class="dropdown-item" href="teacheraddstudent.php">Add Student</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown" style="color:#FFFFFF;">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="color:#FFFFFF;">Subject Management</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="newcourse.php">Create New Course</a>
+                    </div>
+                </li>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item" style="margin-left: 1800%">
+                        <a class="nav-link" style="color:#FFFFFF;" href="../logout.php">Logout</a>
+                    </li>
+                </ul>
+            </ul>
+        </nav>
+    </div>
+    <div style="margin: 15% 10% 25% 10%; ">
+        <form action="" method="POST">
+            <div style="background-color:#282D3D; color:#FFFFFF; padding:1rem; border-radius:20px;">
+            <input type="text" name="courseid" class="form-control" placeholder="Course ID" required>
+            <br>
+            <button  type="submit" id="submit" name="search" class="btn btn-dark" style="color:#FFFFFF; margin-left: 0.3%;" >Search</button>
+            <br>
+            <br>
+        </form>
+            <table class="table table-striped">
+            <thead>
+            <tr style="color:#FFFFFF;">
+                <th>Course Name</th>
+                <th>Teacher</th>
+                <th>Student ID</th>
+                <th>Student Name</th>
+                <th>Behavior Good</th>
+                <th>Behavior Bad</th>
+                <th>Behavior Rate</th>
+                <th></th>
+            </tr>
+    </div>
+</body>
+</html>
---
+++ ui/teacher/newcourse.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>CAAS TEACHER</title>
+</head>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Marvel');
+body {
+    font-family: 'Marvel', sans-serif;
+}
+</style>
+<body style="background-color:#A6A6A6;">
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color:#282D3D; color:#FFFFFF;">
+            <ul class="nav justify-content-center">
+            <a class="navbar-brand" href="teacherhome.php" style="color:#FFFFFF;">CAAS</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="color:#FFFFFF;">Student Management</a>
+                    <div class="dropdown-menu" style="color:#FFFFFF;">
+                        <a class="dropdown-item" href="behavior.php">Behavior Management</a>
+                        <a class="dropdown-item" href="score.php">Score Management</a>
+                        <a class="dropdown-item" href="teacheraddstudent.php">Add Student</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown" style="color:#FFFFFF;">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="color:#FFFFFF;">Subject Management</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="newcourse.php">Create New Course</a>
+                    </div>
+                </li>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item" style="margin-left: 1800%">
+                        <a class="nav-link" style="color:#FFFFFF;" href="../logout.php">Logout</a>
+                    </li>
+                </ul>
+            </ul>
+        </nav>
+    </div>
+    <div style="margin: 15% 25% 25% 25%;">
+        <form action="" method="POST">
+            <div style="background-color:#282D3D; color:#FFFFFF; padding:1rem; border-radius:20px;">
+            <div class="container">
+                <div class="col-xs-4">
+                    <label>Course ID</label>
+                    <input type="text" name="courseid" class="form-control" placeholder="Enter Course ID" required>
+                </div>
+            </div>
+            <br>
+            <div class="container">
+                <div class="col-xs-4">
+                    <label>Course Name</label>
+                    <input type="text" name="coursename" class="form-control" placeholder="Enter Teacher" required>
+                </div>
+            </div>
+            <br>
+            <div class="container">
+                <div class="col-xs-4">
+                    <label>Teacher</label>
+                    <input type="text" name="teacher" class="form-control" placeholder="Enter Teacher" required>
+                </div>
+            </div>
+            <br>
+            <div class="container">
+                <div class="col-xs-4">
+                    <label>Section</label>
+                    <input type="text" name="sec" class="form-control" placeholder="Enter Section" required>
+                </div>
+            </div>
+            <br>
+            <div class="container">
+                <div class="col-xs-4">
+                    <label>Credit</label>
+                    <br>
+                    <select name="credit" required>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="2">3</option>
+                        <option value="2">4</option>                                              
+                    </select>
+                </div>
+            </div>
+            <br>
+            <div class="container">
+                <div class="col-xs-4">
+                    <label>Year</label>
+                    <br>
+                    <select name="year" required>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="2">3</option>
+                        <option value="2">4</option>                                             
+                    </select>
+                </div>
+            </div>
+            <br>
+            <div class="container">
+                <div class="col-xs-4">
+                    <label>Semester</label>
+                    <br>
+                    <select name="semester" required>
+                        <option value="1">1</option>
+                        <option value="2">2</option>                                              
+                    </select>
+                </div>
+            </div>
+            <br> 
+            <button  type="submit" id="submit" class="btn btn-dark" style="color:#FFFFFF; margin-left: 2%;" >Create</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
